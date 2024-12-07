87f06458853536f7b75a133b85d762c00f2af034
Richard <richard@diffusionapps.com>
2024-12-07 21:46:38 +0200

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -6,6 +6,9 @@ self.__uv$config = {
         return encodeURIComponent(
             str
                 .toString()
+                .split('')
+                .map((char, ind) => (ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 3) : char))
+                .join('')
         );
     },
     decodeUrl: function decode(str) {
@@ -14,6 +17,9 @@ self.__uv$config = {
 
         return (
             decodeURIComponent(input)
+            .split('')
+                .map((char, ind) => (ind % 2 ? String.fromCharCode(char.charCodeAt(0) ^ 3) : char))
+                .join('') + (search.length ? '?' + search.join('?') : '')
         );
     },
     handler: '/uv/uv.handler.js',
