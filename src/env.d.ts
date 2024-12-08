541673a96954d3cda998a722d08c334d5cf77ca7
Richard <richard@diffusionapps.com>
2024-12-08 07:18:39 +0200

    Delete src directory

--- src/env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="../.astro/env.d.ts" />
-/// <reference types="astro/client" />
-/// <reference types="@titaniumnetwork-dev/ultraviolet/client" />
-declare var search: (input: string, template: string) => any;
-type Suggestion = {
-    phrase: string;
-};
-declare var BareMux: any;
-declare var EpxMod: any;
